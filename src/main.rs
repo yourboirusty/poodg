79d221b6c9b51682bc25a42e3a3e79277e69d553
Kornel Koszela <kornel.koszela@gmail.com>
2024-09-20 10:13:53 +0200

    chore: refactor

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,17 @@ pub mod controls;
 pub mod game;
 pub mod graphics;
 
+#[cfg(target_os = "none")]
+mod embed;
 #[cfg(not(any(target_os = "none", target_os = "unknown")))]
 mod native;
-
 #[cfg(target_arch = "wasm32")]
 mod wasm;
 
 #[cfg(target_os = "none")]
-mod embed;
-#[cfg(target_os = "none")]
 use bsp::entry;
 #[cfg(target_os = "none")]
 use rp_pico as bsp;
-
-#[cfg(target_os = "none")]
-use defmt::*;
-#[cfg(target_os = "none")]
-use defmt_rtt as _;
-
-#[cfg(target_os = "none")]
-use embed::embed_main;
-
-#[cfg(target_os = "none")]
-use panic_probe as _;
 
 use embedded_graphics::image::Image;
 use embedded_graphics::pixelcolor::BinaryColor;
@@ -50,7 +38,10 @@ fn main() {
 #[cfg(target_os = "none")]
 #[entry]
 fn main() -> ! {
-    info!("WAEO");
+    use defmt::*;
+    use defmt_rtt as _;
+    use embed::embed_main;
+    use panic_probe as _;
 
     embed_main()
 }
